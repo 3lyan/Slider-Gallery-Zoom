da109fbdfc4db8d8a86c2b3820fe4a343bfccc28
m3lyan96 <m3lyan1996@gmail.com>
2022-05-26 02:27:59 +0200

    First Release

---
+++ elian_slider/src/main/java/com/elian/gallery_zoom/SliderFullImagesActivity.kt
@@ -0,0 +1,64 @@
+package com.elian.gallery_zoom
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat.startActivity
+import androidx.viewpager2.widget.ViewPager2
+
+
+class SliderFullImagesActivity : AppCompatActivity() {
+
+    private lateinit var imageClose:ImageView
+    private lateinit var vpImages:ViewPager2
+    private lateinit var tvCount:TextView
+    private var position:Int = 0
+    private var images:ArrayList<String>? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_slider_full_show)
+        imageClose = findViewById(R.id.image_close)
+        vpImages = findViewById(R.id.vp_images)
+        tvCount = findViewById(R.id.tv_count)
+        receiveData()
+        handleClicks()
+        handleViewPager()
+    }
+
+    private fun receiveData(){
+        position = intent.getIntExtra("position",0)
+        images = intent.getStringArrayListExtra("images")
+    }
+
+    private fun handleClicks(){
+        imageClose.setOnClickListener { onBackPressed() }
+    }
+
+    @SuppressLint("SetTextI18n")
+    private fun handleViewPager(){
+        val adapter = images?.let { SliderFullImagesAdapter(it) }
+        vpImages.adapter = adapter
+        vpImages.currentItem = position
+        tvCount.text = "${position+1} ${getString(R.string.of)} ${images?.size}"
+        vpImages.requestDisallowInterceptTouchEvent(true)
+        val myPageChangeCallback = object : ViewPager2.OnPageChangeCallback() {
+            override fun onPageSelected(position: Int) {
+                tvCount.text = "${position+1} ${getString(R.string.of)} ${images?.size}"
+            }
+        }
+        vpImages.registerOnPageChangeCallback(myPageChangeCallback)
+        vpImages.reduceDragSensitivity()
+    }
+
+}
+fun startFullImageSlider(context: Context,imageArrayList: ArrayList<String>,position:Int){
+    val intent = Intent(context,SliderFullImagesActivity::class.java)
+    intent.putExtra("position",position)
+    intent.putExtra("images",imageArrayList)
+    startActivity(context,intent,null)
+}
---
+++ elian_slider/src/main/java/com/elian/gallery_zoom/SliderFullImagesAdapter.kt
@@ -0,0 +1,33 @@
+package com.elian.gallery_zoom
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+
+class SliderFullImagesAdapter(private val dataList: ArrayList<String>): RecyclerView.Adapter<SliderFullImagesAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val layoutInflater = LayoutInflater.from(parent.context)
+        val view: View = layoutInflater.inflate(R.layout.item_full_slider_show,parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return dataList.size
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        Glide.with(holder.itemView).load(dataList[position]).into(holder.imageView)
+    }
+
+
+    inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        var imageView : ImageView = view.findViewById(R.id.place_image)
+
+    }
+
+}
+
